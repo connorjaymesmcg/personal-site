f22cf1eba26fb7947303037977ff5c83900a196a
Connor McGinnis <connorjaymesmcg@gmail.com>
2020-04-17 07:10:43 -0400

    responsive navigation fully functional

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 console.log("working");
 let nav = document.querySelector("nav");
 let navTop = nav.offsetTop;
-
 function stickyScroll() {
   if (window.scrollY >= navTop) {
     document.body.style.paddingTop = nav.offsetHeight + "px";
@@ -11,31 +10,39 @@ function stickyScroll() {
     document.body.classList.remove("sticky");
   }
 }
-
 window.addEventListener("scroll", stickyScroll);
-
+const toggleAnimation = (links) => {
+  links.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.3
+      }s`;
+    }
+  });
+};
 const navSlide = () => {
   const respNav = document.querySelector(".resp-nav");
   const mainNav = document.querySelector(".main-nav");
   const navLinks = document.querySelectorAll(".main-nav li");
-
   //Toggle Nav
   respNav.addEventListener("click", () => {
     mainNav.classList.toggle("nav-active");
-    console.log('responsive nav is active')
-    navLinks.forEach((link, index) => {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-        0.3}s`;
-      link.addEventListener('click', () => {
-        mainNav.classList.toggle('nav-active');
-        console.log('navigation link has been clicked')
-        respNav.classList.toggle('toggle')
-      });
-    });
+    console.log("responsive nav is active");
+    toggleAnimation(navLinks);
     //Burger Animation
     respNav.classList.toggle("toggle");
     console.log("clicked burger");
   });
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      console.log("link clicked:", link);
+      mainNav.classList.toggle("nav-active");
+      toggleAnimation(navLinks);
+      console.log("navigation link has been clicked");
+      respNav.classList.toggle("toggle");
+    });
+  });
 };
-
 navSlide();
