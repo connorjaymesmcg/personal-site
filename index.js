7370fe1d890e72b4eacf8d15f62a91f28d1e25b2
Connor McGinnis <connorjaymesmcg@gmail.com>
2020-05-13 07:52:07 -0400

    added social links in header (placeholders)

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-console.log("working");
-let nav = document.querySelector("nav");
+console.log('working');
+let nav = document.querySelector('nav');
 let navTop = nav.offsetTop;
 function stickyScroll() {
   if (window.scrollY >= navTop) {
-    document.body.style.paddingTop = nav.offsetHeight + "px";
-    document.body.classList.add("sticky");
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('sticky');
   } else {
     document.body.style.paddingTop = 0;
-    document.body.classList.remove("sticky");
+    document.body.classList.remove('sticky');
   }
 }
-window.addEventListener("scroll", stickyScroll);
+window.addEventListener('scroll', stickyScroll);
 const toggleAnimation = (links) => {
   links.forEach((link, index) => {
     if (link.style.animation) {
-      link.style.animation = "";
+      link.style.animation = '';
     } else {
       link.style.animation = `navLinkFade 0.5s ease forwards ${
         index / 7 + 0.3
@@ -23,25 +23,25 @@ const toggleAnimation = (links) => {
   });
 };
 const navSlide = () => {
-  const respNav = document.querySelector(".resp-nav");
-  const mainNav = document.querySelector(".main-nav");
-  const navLinks = document.querySelectorAll(".main-nav li");
+  const respNav = document.querySelector('.resp-nav');
+  const mainNav = document.querySelector('.main-nav');
+  const navLinks = document.querySelectorAll('.main-nav li');
   //Toggle Nav
-  respNav.addEventListener("click", () => {
-    mainNav.classList.toggle("nav-active");
-    console.log("responsive nav is active");
+  respNav.addEventListener('click', () => {
+    mainNav.classList.toggle('nav-active');
+    console.log('responsive nav is active');
     toggleAnimation(navLinks);
     //Burger Animation
-    respNav.classList.toggle("toggle");
-    console.log("clicked burger");
+    respNav.classList.toggle('toggle');
+    console.log('clicked burger');
   });
   navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      console.log("link clicked:", link);
-      mainNav.classList.toggle("nav-active");
+    link.addEventListener('click', () => {
+      console.log('link clicked:', link);
+      mainNav.classList.toggle('nav-active');
       toggleAnimation(navLinks);
-      console.log("navigation link has been clicked");
-      respNav.classList.toggle("toggle");
+      console.log('navigation link has been clicked');
+      respNav.classList.toggle('toggle');
     });
   });
 };
