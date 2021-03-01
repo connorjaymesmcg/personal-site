973bc37e016aa37d6eca2ad8b2eb0119ddfb00f4
Connor McGinnis <connorjaymesmcg@gmail.com>
2021-03-01 08:35:34 -0500

    mobile redesign, removed unused components: slideshow

--- index.js
+++ index.js
@@ -1,64 +1,56 @@
+const header = document.querySelector('header');
+const navTop = document.querySelector('.nav-wrapper');
+const navHeight = navTop.getBoundingClientRect().height;
+const navActive = document.querySelector('.nav-active');
+const mainNav = document.querySelector('.main-nav');
 
-const nav = document.querySelector('.nav-wrapper');
-const navTop = nav.offsetTop;
-const toggleButton = document.querySelector('.resp-nav')
+console.log(navHeight);
 
-toggleButton.addEventListener('click', () => {
-  if (toggleButton.style.animation) {
-    toggleButton.style.animation = '';
-  } else {
-    toggleButton.style.animation = `navToggleAnimation 0.3s ease forwards`
-  }
-})
+// const stickyNav = function (entries) {
+//   const [entry] = entries // desctruct first element of entires array in order to access entries of intersectionObserverAPI
+//   if (!entry.isIntersecting) nav.classList.add('sticky')
+//   else nav.classList.remove('sticky')
+// }
 
-document.addEventListener('DOMContentLoaded', function () {
-  new Splide('#image-slider', {
-    cover: true,
-    rewind: true,
-    perPage: 1,
-    heightRatio: 0.5,
-  }).mount();
-});
+const toggleButton = document.querySelector('.resp-nav');
 
-
-const stickyScroll = () => {
-  if (window.scrollY >= navTop) {
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('sticky');
-  } else {
-    document.body.style.paddingTop = 0;
-    document.body.classList.remove('sticky');
-  }
-};
-
-document.addEventListener('scroll', stickyScroll);
-document.addEventListener('touchmove', stickyScroll);
+// document.addEventListener('scroll', stickyScroll);
+// document.addEventListener('touchmove', stickyScroll);
 
 const toggleAnimation = (links) => {
   links.forEach((link, index) => {
     if (link.style.animation) {
       link.style.animation = '';
     } else {
-      link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3
-        }s`;
+      link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.4}s`;
     }
   });
 };
 
-
+window.addEventListener('load', () => {
+  console.log('page loaded');
+  mainNav.style.display = 'none';
+});
 
 const navSlide = () => {
   const respNav = document.querySelector('.resp-nav');
-  const mainNav = document.querySelector('.main-nav');
   const navLinks = document.querySelectorAll('.main-nav li');
-
   //Toggle Nav
   respNav.addEventListener('click', () => {
-    mainNav.classList.toggle('nav-active');
-    document.documentElement.classList.toggle('noScroll')
-    toggleAnimation(navLinks);
-    //Burger Animation
-    respNav.classList.toggle('toggle');
+    if (!mainNav.classList.contains('nav-active')) {
+      mainNav.style.display = 'flex';
+      mainNav.classList.toggle('nav-active');
+      document.documentElement.classList.toggle('noScroll');
+      toggleAnimation(navLinks);
+      //Burger Animation
+      respNav.classList.toggle('toggle');
+    } else {
+      mainNav.classList.toggle('nav-active');
+      document.documentElement.classList.toggle('noScroll');
+      respNav.classList.toggle('toggle');
+      toggleAnimation(navLinks);
+      mainNav.style.display = 'none';
+    }
   });
 
   navLinks.forEach((link) => {
